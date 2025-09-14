8ab818df581b63ba31ec4cbe540d1ce017a0503e
Shubhneet Kumar <shubhneetkumar2004@gmail.com>
2025-09-14 21:54:04 +0530

    completed text splitters

---
+++ text-splitters/text_structure_based.py
@@ -0,0 +1,54 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
+
+text1 = """
+Space exploration has led to incredible scientific discoveries. From landing on the Moon to exploring Mars, humanity continues to push the boundaries of what’s possible beyond our planet.
+
+These missions have not only expanded our knowledge of the universe but have also contributed to advancements in technology here on Earth. Satellite communications, GPS, and even certain medical imaging techniques trace their roots back to innovations driven by space programs.
+"""
+
+text2 = """
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade  # Grade is a float (like 8.5 or 9.2)
+
+    def get_details(self):
+        return self.name"
+
+    def is_passing(self):
+        return self.grade >= 6.0
+
+
+# Example usage
+student1 = Student("Aarav", 20, 8.2)
+print(student1.get_details())
+
+if student1.is_passing():
+    print("The student is passing.")
+else:
+    print("The student is not passing.")
+
+"""
+
+# Initialize the splitter
+splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.PYTHON,
+    chunk_size=300,
+    chunk_overlap=0,
+)
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=0,
+)
+
+
+# Perform the split
+chunks1 = splitter.split_text(text1)
+chunks2 = splitter.split_text(text2)
+
+
+print(len(chunks1))
+print(chunks1)
+print(len(chunks2))
+print(chunks2[1])
